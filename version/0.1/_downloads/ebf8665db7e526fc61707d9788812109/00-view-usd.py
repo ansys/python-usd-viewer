35dcdd9dd645ea7369a86c762e2f94f2afa2ab37
AlejandroFernandezLuces <AlejandroFernandezLuces@users.noreply.github.com>
2026-09-03 07:23:42 +0000

    DOC: update development documentation 08ec655bfbe4c56dee15174805874703e8b4ab1e

---
+++ version/0.1/_downloads/ebf8665db7e526fc61707d9788812109/00-view-usd.py
@@ -0,0 +1,37 @@
+# Copyright (C) 2025 - 2026 ANSYS, Inc. and/or its affiliates.
+# SPDX-License-Identifier: MIT
+#
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+"""
+.. _ref_plain_usd_view:
+
+=============================
+Plain usage of the USD Viewer
+=============================
+
+This example shows the plain usage of the USD Viewer.
+"""
+from ansys.tools.usdviewer.viewer import USDViewer
+
+# Load USD file
+path = r"display_color.usda"
+viewer = USDViewer(title="USD Viewer", size=(800, 800))
+viewer.load_usd(path)
+viewer.show()
---
+++ version/stable/_downloads/83049b047492b1094090dd18e8147901/01-vtk-asset-resolver.py
@@ -0,0 +1,39 @@
+# Copyright (C) 2025 - 2026 ANSYS, Inc. and/or its affiliates.
+# SPDX-License-Identifier: MIT
+#
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+"""
+.. _ref_vtk_asset_resolver_example:
+
+=======================================
+Viewing a mixed USD file with VTK asset
+=======================================
+
+This example shows how to view a USD file that contains a VTK asset.
+"""
+from ansys.tools.usdviewer.viewer import USDViewer
+
+
+vtk_asset_path = r"display_color_vtk.usda"
+# Initialize the USD Viewer.
+viewer = USDViewer(title="USD Viewer", size=(800, 800))
+# Load USD file with VTK asset.
+viewer.load_usd(vtk_asset_path)
+viewer.show()
